5e47bc4bc98e4f80a0c7b2764f3877e1038a3ba7
Waqar Amin <vacaramin86@gmail.com>
2023-06-20 02:24:55 +0500

    login api complete

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,3 +24,29 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
     })
 
 })
+// Login User =? api/v1/login
+
+exports.loginUser = catchAsyncErrors (async (req, res, next)=>{
+    const {email, password}= req.body;
+    //check if email and password is entered by user
+    if(!email || !password){
+        return next(new ErrorHandler('Please enter email and Password', 400));
+    }
+    //Finding user in Database
+    const user = await User.findOne({email}).select('+password');
+    if (!user){
+        return next(new ErrorHandler('Invalid Email or Password', 401));
+    }
+    const isPasswordMatched = await user.comparePassword(password);
+    if(!isPasswordMatched){
+        return next (new ErrorHandler('Invalid Email or Password'))
+    }
+    const token = user.getJwtToken();
+    res.status(200).json(
+        {
+            success: true,
+            token
+        }
+    )
+
+})
