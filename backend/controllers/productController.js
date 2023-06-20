d70f0d0db24f8f61eea3ff7be87e6afe4cdeac68
Waqar Amin <vacaramin86@gmail.com>
2023-06-20 07:37:46 +0500

    User in product model has been added

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,9 +5,12 @@ const ErrorHandler = require('../utils/errorHandler');
 
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
 const APIFeatures = require('../utils/apiFeatures')
+
+
 exports.newProduct = catchAsyncErrors (async (req, res, next) => {
   
-  
+  req.body.user = req.user.id;
+
   const product = await products.create(req.body) ;
   
   res.status(201).json({
