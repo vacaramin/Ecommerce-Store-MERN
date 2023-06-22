a74ada8147efa47b5cdf05f52f48bc04cdbe6f57
Waqar Amin <vacaramin86@gmail.com>
2023-06-23 03:02:32 +0500

    Order controller has been added

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,37 @@
+const Order = require('../models/order')
+const Product = require('../models/product')
+
+const ErrorHandler = require('../utils/errorHandler')
+const catchAsyncError = require('../middlewares/catchAsyncErrors')
+
+// Create a new Order /api/v1/order/new
+
+exports.newOrder = catchAsyncError(async (req, res, next) => {
+
+    const {
+        orderItems,
+        shippingInfo,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
+        paymentInfo
+    } = req.body
+    const order = await Order.create({
+        orderItems,
+        shippingInfo,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
+        paymentInfo,
+        paidAt: Date.now(),
+        user: req.user._id
+    })
+
+    req.send(200).json({
+        success: true,
+        order
+    })
+
+})
