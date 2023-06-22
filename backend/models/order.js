d9d500581dc2733f50119820cce5e957c7ce0dd8
Waqar Amin <vacaramin86@gmail.com>
2023-06-23 02:48:19 +0500

    Order model has been added

---
+++ backend/models/order.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const orderSchema = mongoose.Schema({
+
+    shippingInfo:{
+        address:{
+            type: String,
+            required: true
+        },
+        city:{
+            type: String,
+            required: true
+        },
+        phoneNo:{
+            type: String,
+            required: true
+        },
+        postalCode:{
+            type: String,
+            required: true
+        },
+        country:{
+            type: String,
+            required: true
+        },
+        user:{
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref:'User'
+        }
+        
+    }
+})
+module.exports = mongoose.model('Order', orderSchema)
