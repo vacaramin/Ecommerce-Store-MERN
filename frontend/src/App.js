3f3b113595d0edb5dca9043eb36cdef77279d97a
Waqar Amin <vacaramin86@gmail.com>
2023-06-30 05:27:38 +0500

    Home component added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './Components/Layout/Header';
 import Footer from './Components/Layout/Footer'
+import Home from './Components/Home/Home';
 
 function App() {
   return (
     <div className='App'>
       <Header />
+      <Home/>
       <Footer />
     </div>
   );
