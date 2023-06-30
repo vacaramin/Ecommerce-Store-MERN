f6036849eb18cfbaa829dc6d5a30872f39cefc06
Waqar Amin <vacaramin86@gmail.com>
2023-07-01 04:30:22 +0500

    Product Reducer complete

--- frontend/src/Components/Reducers/ProductReducers.jsx
+++ frontend/src/Components/Reducers/ProductReducers.jsx
@@ -1,9 +1,36 @@
 
+import {ALL_PRODUCTS_FAIL,
+    ALL_PRODUCTS_SUCCESS,
+    ALL_PRODUCTS_REQUEST,
+    CLEAR_ERRORS
+} from '../Constants/ProductConstants'
 
-export const ProductReducer = (state[{products:[]}, action =>{
+export const ProductReducer = (state = {products:[]}, action) =>{
     switch(action.type){
-        
+        case ALL_PRODUCTS_REQUEST:
+            return {
+                loading: true,
+                products: []
+            }
+        case ALL_PRODUCTS_SUCCESS:
+            return {
+                loading: false,
+                products: action.payload.products,
+                productsCount: action.payload.productCount
+            }
+        case ALL_PRODUCTS_FAIL:
+            return {
+                loading: false,
+                error: action.payload
+            }
+
+        case CLEAR_ERRORS:
+            return {
+            ...state,
+            error: null
+            }
+    
         default:
             return state
     }
-}])
+}
--- frontend/src/Store.js
+++ frontend/src/Store.js
@@ -2,8 +2,9 @@ import {combineReducers, applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension';
 
+import { ProductReducer } from './Components/Reducers/ProductReducers'
 const reducer = combineReducers({
-
+    product: ProductReducer
 })
 
 let initialState = {};
