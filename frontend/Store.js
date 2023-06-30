ee3a2d86e7df6befccbac0452a063eae2ea43ce7
Waqar Amin <vacaramin86@gmail.com>
2023-07-01 03:58:48 +0500

    store.js initial changes made

--- frontend/Store.js
+++ frontend/Store.js
@@ -1,3 +1,13 @@
-import {createStore, combineReducers, applyMiddleware } from 'redux';
+import {configureStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import {composeWithDevTools} from 'redux-devtools-extension'
+import {composeWithDevTools} from 'redux-devtools-extension';
+
+const reducer = combineReducers({
+
+})
+
+let initialState = {};
+const middleware = [thunk];
+
+const store = configureStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)));
+export default store;
