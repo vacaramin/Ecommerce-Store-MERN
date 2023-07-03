1b80a8f8fdd12456565ced3c36c062d38f0a71a8
Waqar Amin <vacaramin86@gmail.com>
2023-07-03 17:32:36 +0500

    bug resolution part 1

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -32,7 +32,7 @@ exports.getProducts = catchAsyncErrors(async (req, res) => {
 
   res.status(200).json({
     success: true,
-    productsCount: productlist.length,
+    count: productlist.length,
     productlist
   });
 
